fe178f7f3f02a39a5e08e963e6f7805a5cd18211
Doucet, Mathieu <doucetm@ornl.gov>
2012-09-24 13:45:46 -0400

    Daemonize workflow manager

--- workflow/sns_post_processing.py
+++ workflow/sns_post_processing.py
@@ -1,5 +1,8 @@
 from workflow_manager import WorkflowManager
 from settings import brokers, icat_user, icat_passcode
+from daemon import Daemon
+import sys
+import os
 
 # List of queues to listen to. Each queue must have a
 # corresponding class in states.py
@@ -15,5 +18,32 @@ queues = ['POSTPROCESS.DATA_READY',
           'POSTPROCESS.INFO',
           'POSTPROCESS.ERROR']
 
-mng = WorkflowManager(brokers, icat_user, icat_passcode, queues, False)
-mng.processing_loop()
+class WorkflowDaemon(Daemon):
+    def run(self):
+        mng = WorkflowManager(brokers, icat_user, icat_passcode, queues, False)
+        mng.processing_loop()
+
+if __name__ == "__main__":
+    # Check whether the output directory exists
+    out_dir = '/var/log/workflow'
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+    
+    daemon = WorkflowDaemon('/tmp/workflow.pid',
+                            stdout=os.path.join(out_dir, 'output.log'),
+                            stderr=os.path.join(out_dir, 'error.log'))
+    
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
