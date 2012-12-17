c6c42260c4c32457d5090d8275b219827827cb5b
Doucet, Mathieu <doucetm@ornl.gov>
2012-12-17 12:00:46 -0500

    Improve simple producer so that it's more flexible

--- test/simple_producer.py
+++ test/simple_producer.py
@@ -5,6 +5,7 @@ import stomp
 import json
 import time
 import sys
+import argparse
 from workflow.settings import brokers, icat_user, icat_passcode
 
 def send(destination, message, persistent='true'):
@@ -21,24 +22,44 @@ def send(destination, message, persistent='true'):
     conn.send(destination=destination, message=message, persistent=persistent)
     conn.disconnect()
     
-def send_msg(i):   
+def send_msg(runid=1234, ipts=5678, 
+             queue='POSTPROCESS.DATA_READY',
+             info=None, error=None):
+    """
+        Send simple ActiveMQ message
+        @param runid: run number (int)
+        @param ipts: IPTS number (int)
+        @param queue: ActiveMQ queue to send message to
+        @param info: optional information message
+        @param error: optional error message
+    """
     data_dict = {"instrument": "HYSA",
-             "ipts": "IPTS-%d" % 5678,
-             "run_number": i,
-             "data_file": 'optional',
-             "information": 'some info',
-             "error": 'some error'}
+             "ipts": "IPTS-%d" % ipts,
+             "run_number": runid,
+             "data_file": 'optional'}
+    
+    # Add info/error as needed
+    if info is not None:
+        data_dict["information"]=info
+    if error is not None:
+        data_dict["error"]=error
 
     data = json.dumps(data_dict)
-    #send('LIVEDATA.UPDATE', data, persistent='true')
-    #send('CATALOG.DATA_READY', data)
-    #send('POSTPROCESS.INFO', data, persistent='true')
-    send('POSTPROCESS.DATA_READY', data, persistent='true')
+    send(queue, data, persistent='true')
     time.sleep(0.1)
     
-runid = 1234
-if len(sys.argv)>1:
-    runid = int(sys.argv[1])
-send_msg(runid)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Workflow manager test producer')
+    parser.add_argument('-r', metavar='runid', type=int, help='Run number (int)', dest='runid')
+    parser.add_argument('-i', metavar='ipts', type=int, help='IPTS number (int)', dest='ipts')
+    parser.add_argument('-q', metavar='queue', help='ActiveMQ queue name', dest='queue')
+    namespace = parser.parse_args()
+    
+    ipts = 5678 if namespace.ipts is None else namespace.ipts
+    runid = 1234 if namespace.runid is None else namespace.runid
+    queue = 'POSTPROCESS.DATA_READY' if namespace.queue is None else namespace.queue
+    
+    print "Sending %s for IPTS-%g, run %g" % (queue, ipts, runid)
+    send_msg(runid, ipts, queue=queue)
     
 
