c4d2585f9409a8a85a3ef5f0df23201a5fe58a26
Doucet, Mathieu <doucetm@ornl.gov>
2013-03-04 09:22:43 -0500

    Use standard login page for admin site, and move it away from the standard location

--- reporting/reporting_app/urls.py
+++ reporting/reporting_app/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 
 from django.contrib import admin
 admin.autodiscover()
+admin.site.login = login_required(admin.site.login)
 
 urlpatterns = patterns('',
     url(r'^$', include('report.urls')),
     url(r'^report/', include('report.urls')),
     url(r'^monitor/', include('monitor.urls')),
     url(r'^users/', include('users.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^database/', include(admin.site.urls)),
 )
