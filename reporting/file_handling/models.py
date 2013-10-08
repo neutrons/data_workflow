b1b2a65340bce9217159f5557c50e3c7485d7660
Doucet, Mathieu <doucetm@ornl.gov>
2013-10-08 11:25:08 -0400

    Add code to delete files when their corresponding database entry is deleted

--- reporting/file_handling/models.py
+++ reporting/file_handling/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from report.models import Instrument, DataRun
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
+from django.dispatch import receiver
+import os
   
 class ReducedImage(models.Model):
+    """
+        Table of image files corresponding to plots of reduced
+        data for a given run.
+    """
     ## DataRun this run status belongs to
     run_id = models.ForeignKey(DataRun)
     ## Data file
@@ -19,3 +25,34 @@ class ReducedImage(models.Model):
             return "No attachment"
 
     file_link.allow_tags = True
+    
+    
+# These two auto-delete files from filesystem when they are unneeded:
+@receiver(models.signals.post_delete, sender=ReducedImage)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+        Deletes file from filesystem
+        when corresponding ReducedImage object is deleted.
+    """
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+
+@receiver(models.signals.pre_save, sender=ReducedImage)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+        Deletes file from filesystem
+        when corresponding ReducedImage object is changed.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = ReducedImage.objects.get(pk=instance.pk).file
+    except ReducedImage.DoesNotExist:
+        return False
+
+    new_file = instance.file
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
