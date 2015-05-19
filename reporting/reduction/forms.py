46cddeec512bf80491527a904f2f0a52fb4af2bf
Doucet, Mathieu <doucetm@ornl.gov>
2015-05-19 11:20:01 -0400

    Add form for Corelli reduction options

--- reporting/reduction/forms.py
+++ reporting/reduction/forms.py
@@ -30,7 +30,51 @@ def _get_choices(instrument):
         logging.error("_get_choices: %s instrument or grouping does not exist\n %s" % (instrument.upper(), sys.exc_value))
     return sorted(form_choices, cmp=lambda x,y:cmp(x[0], y[0]))
 
-class ReductionConfigurationDGSForm(forms.Form):
+class BaseReductionConfigurationForm(forms.Form):
+    """
+        Base class for reduction form
+    """
+    def __init__(self, *args, **kwargs):
+        super(BaseReductionConfigurationForm, self).__init__(*args, **kwargs)
+
+    def set_instrument(self, instrument):
+        """
+            Populate instrument-specific options.
+            @param instrument: instrument short name
+        """
+        pass
+
+    def to_db(self, instrument_id, user=None):
+        """
+            Store the form data
+
+            @param instrument_id: Instrument object
+            @param user: user that made the change
+        """
+        for key in self._template_list:
+            try:
+                if key in self.cleaned_data:
+                    # Make sure we treat booleans properly
+                    if type(self.cleaned_data[key])==bool and self.cleaned_data[key] is False:
+                        value = ''
+                    else:
+                        value = str(self.cleaned_data[key])
+                else:
+                    value = ''
+                view_util.store_property(instrument_id, key, value, user=user)
+            except:
+                logging.error("BaseReductionConfigurationForm.to_db: %s" % sys.exc_value)
+
+    def to_template(self):
+        template_dict = {}
+        for key in self._template_list:
+            if key in self.cleaned_data:
+                template_dict[key] = str(self.cleaned_data[key])
+            else:
+                template_dict[key] = ''
+        return template_dict
+    
+class ReductionConfigurationDGSForm(BaseReductionConfigurationForm):
     """
         Generic form for DGS reduction instruments
     """
@@ -55,41 +99,31 @@ class ReductionConfigurationDGSForm(forms.Form):
             @param instrument: instrument short name
         """
         self.fields['grouping'].choices = _get_choices(instrument)
-        
-    def to_db(self, instrument_id, user=None):
-        """
-            Store the form data
-            
-            @param instrument_id: Instrument object
-            @param user: user that made the change
-        """
-        for key in self._template_list:
-            try:
-                if key in self.cleaned_data:
-                    # Make sure we treat booleans properly
-                    if type(self.cleaned_data[key])==bool and self.cleaned_data[key] is False:
-                        value = ''
-                    else:
-                        value = str(self.cleaned_data[key])
-                else:
-                    value = ''
-                view_util.store_property(instrument_id, key, value, user=user)
-            except:
-                logging.error("ReductionConfigurationDGSForm.to_db: %s" % sys.exc_value)
 
-    def to_template(self):
-        template_dict = {}
-        for key in self._template_list:
-            if key in self.cleaned_data:
-                template_dict[key] = str(self.cleaned_data[key])
-            else:
-                template_dict[key] = ''
-        return template_dict
+
+class ReductionConfigurationCorelliForm(BaseReductionConfigurationForm):
+    """
+        Generic form for Corelli reduction instruments
+    """
+    mask = forms.CharField(required=False, initial='')
+    plot_requests = forms.CharField(required=False, initial='', widget=forms.TextInput(attrs={'class' : 'font_resize'}))
+    ub_matrix_file = forms.CharField(required=False, initial='', widget=forms.TextInput(attrs={'class' : 'font_resize'}))
+    vanadium_flux_file = forms.CharField(required=False, initial='', widget=forms.TextInput(attrs={'class' : 'font_resize'}))
+    vanadium_SA_file = forms.CharField(required=False, initial='', widget=forms.TextInput(attrs={'class' : 'font_resize'}))
+    useCC = forms.BooleanField(required=False)
+
+    ## List of field that are used in the template
+    _template_list = ['mask', 'plot_requests', 'ub_matrix_file', 'vanadium_flux_file',
+                      'vanadium_SA_file', 'useCC']
+    
+    def __init__(self, *args, **kwargs):
+        super(ReductionConfigurationCorelliForm, self).__init__(*args, **kwargs)
+
 
 def validate_integer_list(value):
     """
         Allow for "1,2,3" and "1-3"
-        
+
         @param value: string value to parse
     """
     # Look for a list of ranges
@@ -110,15 +144,15 @@ class MaskForm(forms.Form):
     tube = forms.CharField(required=False, initial='', validators=[validate_integer_list])
     pixel = forms.CharField(required=False, initial='', validators=[validate_integer_list])
     remove = forms.BooleanField(required=False, initial=False)
-    
+
     @classmethod
     def to_tokens(cls, value):
         """
             Takes a block of Mantid script and extract the
             dictionary argument. The template should be like
-            
+
             MaskBTPParameters({'Bank':'', 'Tube':'', 'Pixel':''})
-            
+
             @param value: string value for the code snippet
         """
         mask_list = []
@@ -132,12 +166,12 @@ class MaskForm(forms.Form):
         except:
             logging.error("MaskForm count not parse a command line: %s" % sys.exc_value)
         return mask_list
-    
+
     @classmethod
     def to_python(cls, mask_list, indent='    '):
         """
             Take a block of Mantid script from a list of mask forms
-            
+
             @param mask_list: list of MaskForm objects
             @param indent: string indentation to add to each line
         """
@@ -149,6 +183,40 @@ class MaskForm(forms.Form):
             if len(command_str)>0:
                 command_list += "%s%s\n" % (indent, command_str)
         return command_list
+
+    @classmethod
+    def to_dict_list(cls, mask_list):
+        """
+            Create a list of mask dictionary from a set of mask forms
+            @param mask_list: list of MaskForm objects
+        """
+        mask_info = []
+        for mask in mask_list:
+            entry_dict = {}
+            if 'bank' in mask.cleaned_data and len(mask.cleaned_data['bank'].strip())>0:
+                entry_dict["Bank"] = str(mask.cleaned_data['bank'])
+            if 'tube' in mask.cleaned_data and len(mask.cleaned_data['tube'].strip())>0:
+                entry_dict["Tube"] = str(mask.cleaned_data['tube'])
+            if 'pixel' in mask.cleaned_data and len(mask.cleaned_data['pixel'].strip())>0:
+                entry_dict["Pixel"] = str(mask.cleaned_data['pixel'])
+            if len(entry_dict)>0:
+                mask_info.append(entry_dict)
+        return mask_info
+    
+    @classmethod
+    def from_dict_list(cls, param_value):
+        """
+            Return a list of dictionaries that is compatible with our form
+            @param param_value: string representation of the dictionary
+        """
+        dict_list = eval(param_value)
+        mask_info = []
+        for mask in dict_list:
+            entry_dict = {}
+            for k in mask.keys():
+                entry_dict[k.lower()] = mask[k]
+            mask_info.append(entry_dict)
+        return mask_info
 
     def __str__(self):
         """
@@ -165,4 +233,4 @@ class MaskForm(forms.Form):
         if len(entry_dict)==0:
             return ""
         return "MaskBTPParameters.append(%s)" % str(entry_dict)
-    
+
