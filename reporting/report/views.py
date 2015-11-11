49cb26607fd72c9e1f34ed7bd5f36c016d1b0f9b
Doucet, Mathieu <doucetm@ornl.gov>
2015-11-11 09:10:13 -0500

    Process data from new Mantid json files.

--- reporting/report/views.py
+++ reporting/report/views.py
@@ -147,14 +147,27 @@ def detail(request, instrument, run_id):
                 json_data = json_data_entry.data
                 data_dict = json.loads(json_data)
                 if 'main_output' in data_dict:
-                    x_values = data_dict['main_output']['x']
-                    y_values = data_dict['main_output']['y']
-                    e_values = data_dict['main_output']['e']
-                    if 'x_label' in data_dict['main_output']:
-                        x_label = data_dict['main_output']['x_label']
-                    if 'y_label' in data_dict['main_output']:
-                        y_label = data_dict['main_output']['y_label']
-                    plot_data = [[x_values[i], y_values[i], e_values[i]] for i in range(len(y_values))]
+                    # Old format
+                    if 'x' in data_dict['main_output']:
+                        x_values = data_dict['main_output']['x']
+                        y_values = data_dict['main_output']['y']
+                        e_values = data_dict['main_output']['e']
+                        if 'x_label' in data_dict['main_output']:
+                            x_label = data_dict['main_output']['x_label']
+                        if 'y_label' in data_dict['main_output']:
+                            y_label = data_dict['main_output']['y_label']
+                        plot_data = [[x_values[i], y_values[i], e_values[i]] for i in range(len(y_values))]
+                    # New format from Mantid
+                    elif 'data' in data_dict['main_output']:
+                        x_values = data_dict['main_output']['data']['1'][0]
+                        y_values = data_dict['main_output']['data']['1'][1]
+                        e_values = data_dict['main_output']['data']['1'][2]
+                        if 'axes' in data_dict['main_output']:
+                            if 'xlabel' in data_dict['main_output']:
+                                x_label = data_dict['main_output']['xlabel']
+                            if 'ylabel' in data_dict['main_output']:
+                                y_label = data_dict['main_output']['ylabel']
+                        plot_data = [[x_values[i], y_values[i], e_values[i]] for i in range(len(y_values))]
         elif not request.GET.get('test', '-1') == '-1':
             plot_data = [[0.008, 0.0048], [0.0082, 0.96], [0.0084, 1], [0.0085, 1.1], [0.0087, 1],
                          [0.0089, 0.96], [0.0091, 1], [0.0092, 1], [0.0094, 1], [0.0096, 0.96],
