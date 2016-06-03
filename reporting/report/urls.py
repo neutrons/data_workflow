4cbe7cb10f1d7fd699a54cb10b6912c5b3787015
Doucet, Mathieu <doucetm@ornl.gov>
2016-06-03 11:07:14 -0400

    fix report urls

--- reporting/report/urls.py
+++ reporting/report/urls.py
@@ -1,17 +1,21 @@
-from django.conf.urls import patterns, url
+#pylint: disable=invalid-name, line-too-long
+"""
+    Define url structure
+"""
+from django.conf.urls import url
+from . import views
 
-urlpatterns = patterns('',
-    url(r'^$', 'report.views.summary', name='summary'),
-    url(r'^processing$', 'report.views.processing_admin', name='processing_admin'),
-    url(r'^(?P<instrument>[\w]+)/$', 'report.views.instrument_summary'),
-    url(r'^(?P<instrument>[\w]+)/update/$', 'report.views.get_instrument_update'),
-    url(r'^(?P<instrument>[\w]+)/(?P<run_id>\d+)/$', 'report.views.detail', name='report_detail'),
-    url(r'^(?P<instrument>[\w]+)/(?P<run_id>\d+)/reduce/$', 'report.views.submit_for_reduction'),
-    url(r'^(?P<instrument>[\w]+)/(?P<run_id>\d+)/catalog/$', 'report.views.submit_for_cataloging'),
-    url(r'^(?P<instrument>[\w]+)/(?P<run_id>\d+)/postprocess/$', 'report.views.submit_for_post_processing'),
-    url(r'^(?P<instrument>[\w]+)/experiment/(?P<ipts>[\w\-\.]+)/update/$', 'report.views.get_experiment_update'),
-    url(r'^(?P<instrument>[\w]+)/experiment/(?P<ipts>[\w\-\.]+)/$', 'report.views.ipts_summary'),
-    
-    url(r'^(?P<instrument>[\w]+)/errors/$', 'report.views.live_errors'),
-    url(r'^(?P<instrument>[\w]+)/errors/update$', 'report.views.get_error_update'),
-)
+urlpatterns = [
+    url(r'^$',                                                      views.summary,                      name='summary'),
+    url(r'^processing$',                                            views.processing_admin,             name='processing_admin'),
+    url(r'^(?P<instrument>[\w]+)/$',                                views.instrument_summary,           name='instrument_summary'),
+    url(r'^(?P<instrument>[\w]+)/update/$',                         views.get_instrument_update,        name='get_instrument_update'),
+    url(r'^(?P<instrument>[\w]+)/(?P<run_id>\d+)/$',                views.detail,                       name='detail'),
+    url(r'^(?P<instrument>[\w]+)/(?P<run_id>\d+)/reduce/$',         views.submit_for_reduction,         name='submit_for_reduction'),
+    url(r'^(?P<instrument>[\w]+)/(?P<run_id>\d+)/catalog/$',        views.submit_for_cataloging,        name='submit_for_cataloging'),
+    url(r'^(?P<instrument>[\w]+)/(?P<run_id>\d+)/postprocess/$',    views.submit_for_post_processing,   name='submit_for_post_processing'),
+    url(r'^(?P<instrument>[\w]+)/experiment/(?P<ipts>[\w\-\.]+)/update/$', views.get_experiment_update, name='get_experiment_update'),
+    url(r'^(?P<instrument>[\w]+)/experiment/(?P<ipts>[\w\-\.]+)/$', views.ipts_summary,                 name='ipts_summary'),
+    url(r'^(?P<instrument>[\w]+)/errors/$',                         views.live_errors,                  name='live_errors'),
+    url(r'^(?P<instrument>[\w]+)/errors/update$',                   views.get_error_update,             name='get_error_update'),
+]
