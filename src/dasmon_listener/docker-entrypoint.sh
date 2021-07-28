3a49fd572f321e27d71b38a4c9a1cc757cdd244d
Donnie Earnest <earnestdl@ornl.gov>
2021-07-28 16:40:00 -0500

    added entrypoint for dasmon

---
+++ src/dasmon_listener/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+  
+until PGPASSWORD=${DATABASE_PASS} psql -h "${DATABASE_HOST}" -U "${DATABASE_USER}" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+  
+>&2 echo "Postgres is up - executing command"
+
+/usr/bin/dasmon_listener start && tail -F /var/log/dasmon_listener.log
