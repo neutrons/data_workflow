6fee441937b7110728d2f273d744a1c55285009f
Donnie Earnest <earnestdl@ornl.gov>
2021-08-12 08:42:09 -0500

    move install to entrypoint

---
+++ src/dasmon_listener/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+  
+until PGPASSWORD=${DATABASE_PASS} psql -h "${DATABASE_HOST}" -U "${DATABASE_USER}" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+  >&2 echo "Postgres is up - executing command"
+
+if [ ! -f /tmp/installed ]; then
+  make clean
+  make dasmonlistener
+fi
+
+cd /var/log
+/usr/bin/dasmon_listener start
+tail -F /var/log/dasmon_listener.log
