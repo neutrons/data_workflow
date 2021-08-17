4e54f8438bc27a2e3114b24da9ff5e596e16e69f
Donnie Earnest <earnestdl@ornl.gov>
2021-08-17 07:36:38 -0500

    fix ip addr

--- src/workflow/database/docker-entrypoint.sh
+++ src/workflow/database/docker-entrypoint.sh
@@ -7,5 +7,4 @@ psql -v ON_ERROR_STOP=1 --username $DATABASE_USER --dbname $DATABASE_NAME <<-EOS
     GRANT ALL PRIVILEGES ON DATABASE $AMQDB_NAME TO $AMQDB_USER;
 EOSQL
 
-echo 'host	workflow 	workflow	128.219.164.181/32	md5' >> /var/lib/postgresql/data/pgdata/pg_hba.conf
-echo 'host	workflow 	workflow	128.219.164.209/32	md5' >> /var/lib/postgresql/data/pgdata/pg_hba.conf
+echo 'host	workflow 	workflow	128.219.164.0/32	md5' >> /var/lib/postgresql/data/pgdata/pg_hba.conf
