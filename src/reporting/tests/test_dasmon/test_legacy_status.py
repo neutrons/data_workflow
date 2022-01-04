ea25b69c0adccc3206e3e71726e4e339fb4d245a
Chen Zhang <zhangc@ornl.gov>
2022-01-04 13:21:30 -0500

    reformat and add unit test for reporting.dasmon.models

--- src/reporting/tests/test_dasmon/test_legacy_status.py
+++ src/reporting/tests/test_dasmon/test_legacy_status.py
@@ -10,6 +10,7 @@ import dasmon
 print(dasmon.__dict__)
 print(httplib2.__version__)
 
+
 @mock.patch("dasmon.models.LegacyURL.objects")
 def test_get_legacy_url(mockLegacyURLObj):
     # patch external dependency
@@ -18,10 +19,12 @@ def test_get_legacy_url(mockLegacyURLObj):
 
     # test the function
     from dasmon.legacy_status import get_legacy_url
-    url_without_domain = get_legacy_url(instrument_id = 1, include_domain = False)
+
+    url_without_domain = get_legacy_url(instrument_id=1, include_domain=False)
     assert url_without_domain == "/test.xyz"
-    url_with_domain = get_legacy_url(instrument_id = 1, include_domain = True)
+    url_with_domain = get_legacy_url(instrument_id=1, include_domain=True)
     assert url_with_domain == "http://neutrons.ornl.gov/test.xyz"
+
 
 @mock.patch("dasmon.legacy_status.get_legacy_url")
 @mock.patch("httplib2.HTTPSConnectionWithTimeout")
@@ -30,19 +33,25 @@ def test_get_ops_status(mockHTTPSConnectionWithTimeout, mockGetLegacyUrl):
     mockHTTPSConnectionWithTimeout.return_value = mock.MagicMock()
     mockHTTPSConnectionWithTimeout.return_value.request = mock.MagicMock()
     mockHTTPSConnectionWithTimeout.return_value.getresponse = mock.MagicMock()
-    mockHTTPSConnectionWithTimeout.return_value.getresponse.return_value.read = mock.MagicMock()
+    mockHTTPSConnectionWithTimeout.return_value.getresponse.return_value.read = (
+        mock.MagicMock()
+    )
     fakeReturnDict = {
         "test1": {"test2": "test3"},
     }
-    mockHTTPSConnectionWithTimeout.return_value.getresponse.return_value.read.return_value = json.dumps(fakeReturnDict)
+    mockHTTPSConnectionWithTimeout.return_value.getresponse.return_value.read.return_value = json.dumps(
+        fakeReturnDict
+    )
     #
     mockGetLegacyUrl = mock.MagicMock()
     mockGetLegacyUrl.return_value = "/test.xyz"
 
     # test the function
     from dasmon.legacy_status import get_ops_status
-    data = get_ops_status(instrument_id = "PG3")
-    assert data == [{'group': 'test1', 'data': [{'key': 'test2', 'value': 'test3'}]}]
 
-if __name__ == '__main__':
+    data = get_ops_status(instrument_id="PG3")
+    assert data == [{"group": "test1", "data": [{"key": "test2", "value": "test3"}]}]
+
+
+if __name__ == "__main__":
     pytest.main([__file__])
