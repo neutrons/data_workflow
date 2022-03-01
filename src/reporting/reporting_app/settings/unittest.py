9892676d8d7e3a838708e1d619b04a3909651ea7
Michael Walsh <walshmm@ornl.gov>
2022-03-01 11:33:40 -0500

    find missing unitest settings, remove base settings that messed with test

--- src/reporting/reporting_app/settings/unittest.py
+++ src/reporting/reporting_app/settings/unittest.py
@@ -2,16 +2,26 @@
 from .base import *  # noqa
 from .base import INSTALLED_APPS
 
+del DATABASES  # noqa: F821
 # use sqllite for unit test runs
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
         "OPTIONS": {
             "timeout": 30000,
         },
     }
 }
+
+DEBUG = True
+
+TIME_ZONE = "America/New_York"
+
+CATALOG_URL = "catalog.test.xyz"
+CATALOG_ID = "test"
+CATALOG_SECRET = "test"
+
+del CACHES  # noqa: F821
 
 # This overrides the default and disables ldap
 AUTHENTICATION_BACKENDS = ("django.contrib.auth.backends.ModelBackend",)
