a52e2f7e561cd24d3d61affe90d2c591ae90f40c
Chen Zhang <zhangc@ornl.gov>
2022-03-07 16:50:58 -0500

    reformat py code

--- src/catalog/catalog_process.py
+++ src/catalog/catalog_process.py
@@ -6,14 +6,14 @@ import stomp
 
 class Listener(stomp.ConnectionListener):
     def on_message(self, frame):
-        if frame.headers['destination'] == '/queue/CATALOG.DATA_READY':
-            conn.send('/queue/CATALOG.STARTED', frame.body)
+        if frame.headers["destination"] == "/queue/CATALOG.DATA_READY":
+            conn.send("/queue/CATALOG.STARTED", frame.body)
             time.sleep(0.1)
-            conn.send('/queue/CATALOG.COMPLETE', frame.body)
+            conn.send("/queue/CATALOG.COMPLETE", frame.body)
         else:
-            conn.send('/queue/REDUCTION_CATALOG.STARTED', frame.body)
+            conn.send("/queue/REDUCTION_CATALOG.STARTED", frame.body)
             time.sleep(0.1)
-            conn.send('/queue/REDUCTION_CATALOG.COMPLETE', frame.body)
+            conn.send("/queue/REDUCTION_CATALOG.COMPLETE", frame.body)
 
 
 if __name__ == "__main__":
@@ -25,10 +25,10 @@ if __name__ == "__main__":
 
     # Start the connection
     conn = stomp.Connection(host_and_ports=BROKERS)
-    conn.set_listener('', Listener())
+    conn.set_listener("", Listener())
     conn.connect(ICAT_USER, ICAT_PASSCODE, wait=True)
-    conn.subscribe(destination='/queue/CATALOG.DATA_READY', id=1, ack='auto')
-    conn.subscribe(destination='/queue/REDUCTION_CATALOG.DATA_READY', id=1, ack='auto')
+    conn.subscribe(destination="/queue/CATALOG.DATA_READY", id=1, ack="auto")
+    conn.subscribe(destination="/queue/REDUCTION_CATALOG.DATA_READY", id=1, ack="auto")
 
     while conn.is_connected():
         time.sleep(1)
