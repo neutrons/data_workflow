eea028817e35599d5be31e778f8efcb18f1eaa73
Pete Peterson <petersonpf@ornl.gov>
2022-03-17 11:27:44 -0400

    Change back to old sql stored procedures

--- src/reporting/pvmon/sql/stored_procs.sql
+++ src/reporting/pvmon/sql/stored_procs.sql
@@ -1,123 +1,110 @@
 -- Function used by DASMON to add PV entries
--- This version works for PGSQL >= 8.4.20
+
 -- Function: "pvUpdate"(character varying, double precision, bigint, bigint)
 
-CREATE OR REPLACE FUNCTION "pvUpdate"(pv_name character varying, input_value double precision, input_status bigint, input_update_time bigint)
-  RETURNS void AS
+CREATE OR REPLACE FUNCTION pvUpdate(pv_name character varying, value double precision, status bigint, update_time bigint)
+RETURNS void AS
 $BODY$DECLARE
-  n_count numeric;
-  n_id bigint;
-  new_value double precision;
-  new_time bigint;
+n_count numeric;
+n_id bigint;
+new_value double precision;
+new_time bigint;
 BEGIN
-  new_value := input_value;
-  new_time := input_update_time;
-  -- Create the parameter name entry if it doesn't already exist
-  SELECT COUNT(*)
+new_value := value;
+new_time := update_time;
+-- Create the parameter name entry if it doesn't already exist
+SELECT COUNT(*)
     FROM pvmon_pvname
     INTO n_count
     WHERE pvmon_pvname.name = pv_name;
-
-  IF n_count = 0 THEN
+IF n_count = 0 THEN
     INSERT INTO pvmon_pvname (name, monitored) VALUES (pv_name, true);
-  END IF;
-
-  -- Get the ID from the parameter name table
-  SELECT id
+END IF;
+-- Get the ID from the parameter name table
+SELECT id
     FROM pvmon_pvname
     INTO n_id
     WHERE pvmon_pvname.name = pv_name;
-
-  -- Add the entry for the new value
-  INSERT INTO pvmon_pv (name_id, value, status, update_time)
-    VALUES (n_id, new_value, input_status, input_update_time);
-
-  -- Cache the latest values
-  SELECT COUNT(*)
+-- Add the entry for the new value
+INSERT INTO pvmon_pv (name_id, value, status, update_time)
+    VALUES (n_id, new_value, status, update_time);
+-- Cache the latest values
+SELECT COUNT(*)
     FROM pvmon_pvcache
     INTO n_count
     WHERE pvmon_pvcache.name_id = n_id;
-
-  IF n_count = 0 THEN
+IF n_count = 0 THEN
     INSERT INTO pvmon_pvcache (name_id, value, status, update_time)
-      VALUES (n_id, input_value, input_status, input_update_time);
-  ELSE
+    VALUES (n_id, value, status, update_time);
+ELSE
     UPDATE pvmon_pvcache
-      SET value=new_value, update_time=new_time
-      WHERE name_id = n_id;
-  END IF;
+    SET value=new_value, update_time=new_time
+    WHERE name_id = n_id;
+END IF;
 END;$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION "pvUpdate"(character varying, double precision, bigint, bigint)
-  OWNER TO postgres;
-
+LANGUAGE plpgsql VOLATILE
+COST 100;
+ALTER FUNCTION pvUpdate(character varying, double precision, bigint, bigint)
+OWNER TO postgres;
 
 -- Function: "pvUpdate"(character varying, character varying, double precision, bigint, bigint)
 
-CREATE OR REPLACE FUNCTION "pvUpdate"(instrument character varying, pv_name character varying, input_value double precision, input_status bigint, input_update_time bigint)
-  RETURNS void AS
+CREATE OR REPLACE FUNCTION pvUpdate(instrument character varying, pv_name character varying, value double precision, status bigint, update_time bigint)
+RETURNS void AS
 $BODY$
 DECLARE
-  n_count  numeric;
-  n_id bigint;
-  n_instrument bigint;
-  new_value double precision;
-  new_time bigint;
+n_count  numeric;
+n_id bigint;
+n_instrument bigint;
+new_value double precision;
+new_time bigint;
 BEGIN
-  new_value := input_value;
-  new_time := input_update_time;
-  -- Create the parameter name entry if it doesn't already exist
-  SELECT COUNT(*)
+new_value := value;
+new_time := update_time;
+-- Create the parameter name entry if it doesn't already exist
+SELECT COUNT(*)
     FROM pvmon_pvname
     INTO n_count
     WHERE pvmon_pvname.name = pv_name;
-
-  IF n_count = 0 THEN
+IF n_count = 0 THEN
     INSERT INTO pvmon_pvname (name, monitored) VALUES (pv_name, true);
-  END IF;
-
-  -- Get the ID from the parameter name table
-  SELECT id
+END IF;
+-- Get the ID from the parameter name table
+SELECT id
     FROM pvmon_pvname
     INTO n_id
     WHERE pvmon_pvname.name = pv_name;
-
-  -- Get the instrument ID
-  SELECT id
+-- Get the instrument ID
+SELECT id
     FROM report_instrument
     INTO n_instrument
     WHERE report_instrument.name = lower(instrument);
-
-  -- Add the entry for the new value
-  INSERT INTO pvmon_pv (instrument_id, name_id, value, status, update_time)
-    VALUES (n_instrument, n_id, new_value, input_status, new_time);
-
-  -- Cache the latest values
-  SELECT COUNT(*)
+-- Add the entry for the new value
+INSERT INTO pvmon_pv (instrument_id, name_id, value, status, update_time)
+    VALUES (n_instrument, n_id, new_value, status, new_time);
+-- Cache the latest values
+SELECT COUNT(*)
     FROM pvmon_pvcache
     INTO n_count
     WHERE pvmon_pvcache.instrument_id=n_instrument AND pvmon_pvcache.name_id = n_id;
-
-  IF n_count = 0 THEN
+IF n_count = 0 THEN
     INSERT INTO pvmon_pvcache (instrument_id, name_id, value, status, update_time)
-      VALUES (n_instrument, n_id, new_value, input_status, new_time);
-  ELSE
+    VALUES (n_instrument, n_id, new_value, status, new_time);
+ELSE
     UPDATE pvmon_pvcache
-      SET value=new_value, update_time=new_time
-      WHERE name_id = n_id AND instrument_id = n_instrument;
-  END IF;
+    SET value=new_value, update_time=new_time
+    WHERE name_id = n_id AND instrument_id = n_instrument;
+END IF;
 END;
 $BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION "pvUpdate"(character varying, character varying, double precision, bigint, bigint)
-  OWNER TO postgres;
-
+LANGUAGE plpgsql VOLATILE
+COST 100;
+ALTER FUNCTION pvUpdate(character varying, character varying, double precision, bigint, bigint)
+OWNER TO postgres;
 
 -- Function: "pvStringUpdate"(character varying, character varying, character varying, bigint, bigint)
 
-CREATE OR REPLACE FUNCTION "pvStringUpdate"(instrument character varying, pv_name character varying, input_value character varying, input_status bigint, input_update_time bigint)
+CREATE OR REPLACE FUNCTION pvStringUpdate(instrument character varying, pv_name character varying, value character varying, status bigint, update_time bigint)
   RETURNS void AS
 $BODY$
 DECLARE
@@ -127,43 +114,37 @@ DECLARE
   new_value character varying;
   new_time bigint;
 BEGIN
-  new_value := input_value;
-  new_time := input_update_time;
+  new_value := value;
+  new_time := update_time;
   -- Create the parameter name entry if it doesn't already exist
   SELECT COUNT(*)
     FROM pvmon_pvname
     INTO n_count
     WHERE pvmon_pvname.name = pv_name;
-
   IF n_count = 0 THEN
     INSERT INTO pvmon_pvname (name, monitored) VALUES (pv_name, true);
   END IF;
-
   -- Get the ID from the parameter name table
   SELECT id
     FROM pvmon_pvname
     INTO n_id
     WHERE pvmon_pvname.name = pv_name;
-
   -- Get the instrument ID
   SELECT id
     FROM report_instrument
     INTO n_instrument
     WHERE report_instrument.name = lower(instrument);
-
   -- Add the entry for the new value
   INSERT INTO pvmon_pvstring (instrument_id, name_id, value, status, update_time)
-    VALUES (n_instrument, n_id, new_value, input_status, new_time);
-
+    VALUES (n_instrument, n_id, new_value, status, new_time);
   -- Cache the latest values
   SELECT COUNT(*)
     FROM pvmon_pvstringcache
     INTO n_count
     WHERE pvmon_pvstringcache.instrument_id=n_instrument AND pvmon_pvstringcache.name_id = n_id;
-
   IF n_count = 0 THEN
     INSERT INTO pvmon_pvstringcache (instrument_id, name_id, value, status, update_time)
-      VALUES (n_instrument, n_id, new_value, input_status, new_time);
+      VALUES (n_instrument, n_id, new_value, status, new_time);
   ELSE
     UPDATE pvmon_pvstringcache
       SET value=new_value, update_time=new_time
@@ -173,5 +154,5 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION "pvStringUpdate"(character varying, character varying, character varying, bigint, bigint)
+ALTER FUNCTION pvStringUpdate(character varying, character varying, character varying, bigint, bigint)
   OWNER TO postgres;
