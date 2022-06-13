e268158bd387f965b3a7bdb8683217324e719ada
Ross Whitfield <whitfieldre@ornl.gov>
2022-06-13 15:29:50 -0400

    Add healthchecks

--- nginx/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# remove default nginx config
-rm -f /etc/nginx/conf.d/default.conf
-
-#https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
-script=/usr/bin/wait-for-it.sh
-chmod +x $script
-bash -c "$script webmon:8000 --timeout=300"
