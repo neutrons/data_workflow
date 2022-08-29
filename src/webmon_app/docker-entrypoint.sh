079348d6759def021c7f8b064cacca860bdafcba
Jose Borreguero <borreguero@gmail.com>
2022-08-29 14:32:33 -0400

    reintroduce preload option

--- src/webmon_app/docker-entrypoint.sh
+++ src/webmon_app/docker-entrypoint.sh
@@ -31,4 +31,4 @@ fi
 # start up web-service
 # entrypoint is python.package:function_name
 # additional command-line arguments via environment variable GUNICORN_CMD_ARGS
-gunicorn reporting.reporting_app.wsgi:application --bind 0.0.0.0:8000
+gunicorn reporting.reporting_app.wsgi:application --preload --bind 0.0.0.0:8000
