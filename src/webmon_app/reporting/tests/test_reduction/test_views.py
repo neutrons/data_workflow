a3da5d3d4da7479b0b92c394b9e94b28123a46b2
Jose Borreguero <borreguero@gmail.com>
2024-10-30 16:02:25 -0400

    Refactor test: Comment out browser inspection code
    Commented out the line that writes response content to a file for browser inspection. This change ensures that the code is only executed when debugging, preventing accidental file creation during regular test runs.
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- src/webmon_app/reporting/tests/test_reduction/test_views.py
+++ src/webmon_app/reporting/tests/test_reduction/test_views.py
@@ -197,7 +197,8 @@ class TestView(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "reduction/configuration_ref_m.html")
-        open("/tmp/junk.html", "wb").write(response.content)  # inspect with the browser
+        # uncomment the following line only when debugging, in order to inspect the response content
+        # open("/tmp/junk.html", "wb").write(response.content)  # inspect with the browser
 
     def test_configuration_change(self):
         # no data
