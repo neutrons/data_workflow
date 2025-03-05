d3c4e8892d83786e1f46ab1efa8826db05090924
Ross Whitfield <whitfieldre1@ornl.gov>
2025-03-06 10:15:28 +1100

    Don't allow editing MonitoredVariable from table
    This was causing it to load to slow as it was creating a dropdown for PVName for every row (there are 100 rows per page) that had ~23000 items in it. Django did this really inefficiently.
    
    You can still edit the entry by first selecting the ID. I also removed the custom form for adding and editing, this allows the dropdown to be created there but since it is just for a single entry it will be fast.

--- src/webmon_app/reporting/pvmon/admin.py
+++ src/webmon_app/reporting/pvmon/admin.py
@@ -1,32 +1,6 @@
 from reporting.pvmon.models import PVName, PV, PVCache, PVString, PVStringCache, MonitoredVariable
 from django.contrib import admin
-from django import forms
-from django.core.exceptions import ValidationError
 import datetime
-
-
-class PVNameCharField(forms.fields.CharField):
-    def to_python(self, value):
-        if isinstance(value, PVName):
-            return value
-
-        if value is None:
-            return value
-
-        try:
-            return PVName.objects.get(name=value)
-        except PVName.DoesNotExist:
-            raise ValidationError("PvName does not exist")
-
-
-class AddForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["pv_name"] = PVNameCharField()
-
-    class Meta:
-        model = MonitoredVariable
-        exclude = ()
 
 
 class PVAdmin(admin.ModelAdmin):
@@ -54,8 +28,6 @@ class PVNameAdmin(admin.ModelAdmin):
 
 class MonitoredVariableAdmin(admin.ModelAdmin):
     list_display = ("id", "instrument", "pv_name", "rule_name")
-    list_editable = ("pv_name", "rule_name")
-    form = AddForm
 
 
 admin.site.register(PVName, PVNameAdmin)
