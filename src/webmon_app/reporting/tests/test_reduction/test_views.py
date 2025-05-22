c2f98576751c87c8389541e41fcb3a213debc3a7
Jose Borreguero <borreguero@gmail.com>
2025-05-22 15:47:47 -0400

    Add help text for ROI options in configuration_ref_m.html (#243)
    * Add help text for ROI options in configuration_ref_m.html
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    * Fix HTML structure for 'Force peak ROI' label in configuration_ref_m.html
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    * Update pull request template to highlight manual testing instructions
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    * Fix ROI label references in configuration_ref_m.html
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    * Remove unnecessary blank line in configuration_ref_m.html
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    * Adjust margin spacing in configuration_ref_m.html for improved layout
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>
    
    ---------
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- src/webmon_app/reporting/tests/test_reduction/test_views.py
+++ src/webmon_app/reporting/tests/test_reduction/test_views.py
@@ -202,7 +202,7 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "reduction/configuration_ref_m.html")
         # uncomment the following line only when debugging, in order to inspect the response content
-        open("/tmp/junk.html", "wb").write(response.content)  # take a look to file:///tmp/junk.html with the browser
+        # open("/tmp/junk.html", "wb").write(response.content)  # take a look to file:///tmp/junk.html with the browser
 
     def test_configuration_change(self):
         # no data
