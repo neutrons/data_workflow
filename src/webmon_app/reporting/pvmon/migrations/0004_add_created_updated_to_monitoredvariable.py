61408b1b8b4b65732b669dddaa413a291efdc773
Darsh <ddarsh5544@gmail.com>
2025-09-10 14:50:36 -0400

    Fix migration conflict

--- src/webmon_app/reporting/pvmon/migrations/0004_add_created_updated_to_monitoredvariable.py
+++ src/webmon_app/reporting/pvmon/migrations/0004_add_created_updated_to_monitoredvariable.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("pvmon", "0002_delete_pvstring"),
+        ("pvmon", "0003_pv_timestamp_pvcache_timestamp_and_more"),
     ]
 
     operations = [
