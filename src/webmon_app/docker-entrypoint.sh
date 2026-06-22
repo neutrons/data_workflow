53736c8ece997f8d0d79c23926ae5f71ffb48568
Kevin A. Tactac <tactacka@ornl.gov>
2026-06-22 15:56:49 -0400

    try fix unhealthy container

--- src/webmon_app/docker-entrypoint.sh
+++ src/webmon_app/docker-entrypoint.sh
@@ -8,14 +8,22 @@ until python -c "import psycopg2; import os; psycopg2.connect(host=os.environ['D
 done
 >&2 echo "Postgres is up - executing command"
 
-# configure the application (pip install done at Docker build time)
-pixi run -e webmon configure-webmon
+# configure the application (pip install done at Docker build time; django-admin is on PATH
+# because this entrypoint runs inside 'pixi run -e webmon' via the container CMD)
+django-admin collectstatic --noinput
+django-admin makemigrations --noinput
+django-admin migrate --fake-initial --noinput
+django-admin migrate --run-syncdb --noinput
+django-admin createcachetable webcache
+django-admin ensure_adminuser --username="${DATABASE_USER}" --email='workflow@example.com' --password="${DATABASE_PASS}"
+django-admin add_stored_procs
+django-admin add_indices
 
 # add test users if not in prod
 >&2 echo "Checking if in prod..."
 if [[ "$DJANGO_SETTINGS_MODULE" != *".prod" ]]; then
   if [[ "${LOAD_INITIAL_DATA}" = "true" ]]; then
-    pixi run -e webmon configure-load-initial-data
+    django-admin loaddata "$(python -c "import reporting, os; print(os.path.join(os.path.dirname(reporting.__file__), 'fixtures', 'db_init.json'))")"
   fi
   if [ -z "$GENERAL_USER_USERNAME" ]; then
     GENERAL_USER_USERNAME="GeneralUser"
